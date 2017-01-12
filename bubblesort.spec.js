466ca47b1ccc22d15109fc86d2732c95f1693fb5
Zohaib Rahman <zohaibr@gmail.com>
2017-01-12 14:43:34 -0500

    Finished BubbleSort

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,4 +1,9 @@
 describe('Bubble Sort', function() {
+  it('it swaps two numbers based on the index', function() {
+    let arr = [8, 7]
+    swap(arr, 0);
+    expect(arr).toEqual([7, 8]);
+  });
 
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
@@ -6,8 +11,15 @@ describe('Bubble Sort', function() {
 
   it('counts the number of swaps in the bubbleSort', function() {
     spyOn(window, 'swap').and.callThrough();
-    bubbleSort([5,2,1,3,5]);
-    expect(swap.calls.count()).toEqual(3);
+    bubbleSort([5, 2, 1, 3, 5]);
+    expect(swap.calls.count()).toEqual(4);
   });
 
+  it ('sorts an array in numerical order', function() {
+    expect(bubbleSort([4, 2, 1, 3, 5])).toEqual([1, 2, 3, 4, 5])
+  });
+
+  it ('sorts an array in numerical order with repeated numbers', function() {
+    expect(bubbleSort([5, 2, 1, 3, 5])).toEqual([1, 2, 3, 5, 5])
+  });
 });
