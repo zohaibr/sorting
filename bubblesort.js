79d41d217fb2bc001a53f4bffbf77dee7d381822
Mike Peritz <mike.peritz@gmail.com>
2017-01-12 13:08:55 -0500

    finished swap fn

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,14 @@
-function bubbleSort() {
-  return [];
+function bubbleSort(arr) {
+  let sortedArr = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+  	if (arr[i] > arr[i + 1]) swap(arr, i)
+  }
+  return arr;
+}
+
+function swap(arr, index) {
+  let val1 = arr[index];
+  let val2 = arr[index + 1];
+  arr[index] = val2;
+  arr[index + 1] = val1;
 }
